e3a04f4237a762c94c3956b44c4d62655a2b412d
Ramya Sree <ramyasree@Ramyas-Laptop.local>
2023-03-19 17:21:44 -0500

    save the universe

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ class Spaceship extends Battle{
     constructor(){
     super()
     
-    this.hulu=20,
+    this.hull=20,
     this.firepower=5,
     this.accuracy=.7;
     }
@@ -18,7 +18,7 @@ class Spaceship extends Battle{
   /*if(this.accuracy>alShipobj.accuracy)
   {
     console.log("Alien Ship Got Attacked ")
-    console.log(alShipobj.hulu - alShipobj.firepower);
+    console.log(alShipobj.hull - alShipobj.firepower);
   }else { }*/
     console.log(alShipobj.name + " got destroyed");
  }
@@ -29,7 +29,7 @@ class Alienships extends Battle {
     constructor(name){
         super();
         this.name=name;
-        this.hulu=Math.floor(Math.random()*2)+4;
+        this.hull=Math.floor(Math.random()*2)+4;
         this.firepower=Math.floor(Math.random()*2)+3;
         this.accuracy=(Math.random()*.2)+.6;
     }
@@ -37,7 +37,7 @@ class Alienships extends Battle {
     if(this.accuracy>spceShip.accuracy)
     {
         console.log("Alien ship Attack Back to Spaceship")
-        spaceship1.hulu = spaceship1.hulu- this.firepower;
+        spaceship1.hull = spaceship1.hull- this.firepower;
     }else{
 
         console.log(this.name + " got destroyed")
@@ -53,13 +53,13 @@ let alienship1=new Alienships("Ship1")
 
 for(let i =0; i<6; i++){
     
-    console.log("Space Ship Hulu: "+spaceship1.hulu);
+    console.log("Space Ship hull: "+spaceship1.hull);
     console.log("Space Ship Fire Power: "+spaceship1.firepower);
     console.log("Space Ship Accuracy: "+spaceship1.accuracy);
 
     j = i +1;
     let alienship1=new Alienships("Alien Ship"+j);
-    console.log(alienship1.name+" Hulu: "+alienship1.hulu);
+    console.log(alienship1.name+" hull: "+alienship1.hull);
     console.log(alienship1.name+" Fire Power: "+alienship1.firepower);
     console.log(alienship1.name+" Accuracy: "+alienship1.accuracy);
     if(alienship1.accuracy > spaceship1.accuracy){
@@ -68,9 +68,13 @@ for(let i =0; i<6; i++){
     else {
         spaceship1.targetAlienship(alienship1);
     }
+
+    if(spaceship1.hull <= 0){
+        break;
+    }
 }
 
-if(spaceship1.hulu <= 0){
+if(spaceship1.hull <= 0){
     console.log("Space Ship destroyed, You lost")
 }else {
     console.log("Alien Ships destroyed, You Won")
